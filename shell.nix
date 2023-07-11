68f308b4e8772489f7ca126126efbd4e8039d0a6
Christopher Miller <hello@chrismiller.xyz>
2023-07-11 00:20:28 -0700

    notion post syncing

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4f91766e8c58501176aa67e34a460e588f9a56fb.tar.gz") {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    git
+    kubectl
+    pre-commit
+    azure-cli
+  ];
+}
